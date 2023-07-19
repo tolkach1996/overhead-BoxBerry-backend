76ede80fecd58841efa573f4344a0a69053b2018
Vladislav Tolkachev <tvladick1996@yandex.ru>
2023-07-20 00:21:59 +0300

    20.07

--- index.js
+++ index.js
@@ -33,122 +33,72 @@ const start = () => {
     }
 }
 
-/*for (let item of table) {
-        for (let k of item.orders) {
-            let newStreing = {
-                "Дата посылки (ГГГГММДД)": item.dataPackage,
-                "Номер заказа в ИМ": k.number,
-                "Объявленная стоимость": k.declaredSum,
-                "Сумма к оплате": item.paySum,
-                "Стоимость доставки": item.deliverySum,
-                "Дата передачи ЗП": k.dataTransfer,
-                "Номер паллеты": '',
-                "Номер акта передачи": '',
-                "Вид доставки": item.typeTransfer,
-                "Код ПВЗ": item.codePWZ,
-                "Код пункта поступления": item.departurePointCode,
-                "ФИО": item.fio,
-                "Номер телефона": item.phone,
-                "Доп. номер телефона": '',
-                "E-mail для оповещений": '',
-                "Наименование организации": '',
-                "Адрес": '',
-                "ИНН": '',
-                "КПП": '',
-                "Расчетный счет": '',
-                "Наименование банка": '',
-                "Кор. счет": '',
-                "БИК": '',
-                "Вес 1-ого места": item.weightPackage,
-                "Вес 2-ого места": '',
-                "Вес 3-ого места": '',
-                "Вес 4-ого места": '',
-                "Вес 5-ого места": '',
-                "Индекс": '',
-                "Город": '',
-                "Адрес получателя": '',
-                "Время доставки, от (ЧЧ:ММ)": '',
-                "Время доставки, до (ЧЧ:ММ)": '',
-                "Дата доставки (ГГГГММДД)": '',
-                "Комментарий": '',
-                "Баркод 1-го места": '',
-                "Баркод 2-го места": '',
-                "Баркод 3-го места": '',
-                "баркод 4-го места": '',
-                "Баркод 5-го места": '',
-                "Тип отпраления ПР": '',
-                "Хрупкий груз для ПР": '',
-                "Оптимизация тарифа ПР": '',
-                "Строгий тип отправления ПР": '',
-                "Длина, см": '',
-                "Ширина, см": '',
-                "Высота, см": '',
-                "Тип упаковки": '',
-                "Запретить изменение упаковки": '',
-                "Тип выдачи": '',
-            }
-            table2.push(newStreing)
-        }
-    }*/
+
 app.post('/downloadConsigmentExcel', (req, res) => {
     const workbook = xlsx.utils.book_new();
     let table = []
     for (let item of req.body?.data) {
-        for (let k of item.orders) {
-            let newStreing = {
-                "Дата посылки (ГГГГММДД)": item.dataPackage,
-                "Номер заказа в ИМ": k.number,
-                "Объявленная стоимость": k.declaredSum,
-                "Сумма к оплате": item.paySum,
-                "Стоимость доставки": item.deliverySum,
-                "Дата передачи ЗП": k.dataTransfer,
-                "Номер паллеты": '',
-                "Номер акта передачи": '',
-                "Вид доставки": item.typeTransfer,
-                "Код ПВЗ": item.codePWZ,
-                "Код пункта поступления": item.departurePointCode,
-                "ФИО": item.fio,
-                "Номер телефона": item.phone,
-                "Доп. номер телефона": '',
-                "E-mail для оповещений": '',
-                "Наименование организации": '',
-                "Адрес": '',
-                "ИНН": '',
-                "КПП": '',
-                "Расчетный счет": '',
-                "Наименование банка": '',
-                "Кор. счет": '',
-                "БИК": '',
-                "Вес 1-ого места": item.weightPackage,
-                "Вес 2-ого места": '',
-                "Вес 3-ого места": '',
-                "Вес 4-ого места": '',
-                "Вес 5-ого места": '',
-                "Индекс": '',
-                "Город": '',
-                "Адрес получателя": '',
-                "Время доставки, от (ЧЧ:ММ)": '',
-                "Время доставки, до (ЧЧ:ММ)": '',
-                "Дата доставки (ГГГГММДД)": '',
-                "Комментарий": '',
-                "Баркод 1-го места": '',
-                "Баркод 2-го места": '',
-                "Баркод 3-го места": '',
-                "баркод 4-го места": '',
-                "Баркод 5-го места": '',
-                "Тип отпраления ПР": '',
-                "Хрупкий груз для ПР": '',
-                "Оптимизация тарифа ПР": '',
-                "Строгий тип отправления ПР": '',
-                "Длина, см": '',
-                "Ширина, см": '',
-                "Высота, см": '',
-                "Тип упаковки": '',
-                "Запретить изменение упаковки": '',
-                "Тип выдачи": '',
+        let declaredSum = 0;
+        if (item.orders.length > 1) {
+            for (let order of item.orders) {
+                declaredSum += order.declaredSum
             }
-            table.push(newStreing)
+            if (declaredSum < 10000) declaredSum = 5
         }
+        else declaredSum = 5
+        let newStreing = {
+            "Дата посылки (ГГГГММДД)": item.dataPackage,
+            "Номер заказа в ИМ": item.orders[0].number,
+            "Объявленная стоимость": declaredSum,
+            "Сумма к оплате": item.paySum,
+            "Стоимость доставки": item.deliverySum,
+            "Дата передачи ЗП": item.dataPackage,
+            "Номер паллеты": '',
+            "Номер акта передачи": '',
+            "Вид доставки": item.typeTransfer,
+            "Код ПВЗ": item.codePWZ,
+            "Код пункта поступления": item.departurePointCode,
+            "ФИО": item.fio,
+            "Номер телефона": item.phone,
+            "Доп. номер телефона": '',
+            "E-mail для оповещений": '',
+            "Наименование организации": '',
+            "Адрес": '',
+            "ИНН": '',
+            "КПП": '',
+            "Расчетный счет": '',
+            "Наименование банка": '',
+            "Кор. счет": '',
+            "БИК": '',
+            "Вес 1-ого места": item.weightPackage,
+            "Вес 2-ого места": '',
+            "Вес 3-ого места": '',
+            "Вес 4-ого места": '',
+            "Вес 5-ого места": '',
+            "Индекс": '',
+            "Город": '',
+            "Адрес получателя": '',
+            "Время доставки, от (ЧЧ:ММ)": '',
+            "Время доставки, до (ЧЧ:ММ)": '',
+            "Дата доставки (ГГГГММДД)": '',
+            "Комментарий": '',
+            "Баркод 1-го места": '',
+            "Баркод 2-го места": '',
+            "Баркод 3-го места": '',
+            "баркод 4-го места": '',
+            "Баркод 5-го места": '',
+            "Тип отпраления ПР": '',
+            "Хрупкий груз для ПР": '',
+            "Оптимизация тарифа ПР": '',
+            "Строгий тип отправления ПР": '',
+            "Длина, см": '',
+            "Ширина, см": '',
+            "Высота, см": '',
+            "Тип упаковки": '',
+            "Запретить изменение упаковки": '',
+            "Тип выдачи": '',
+        }
+        table.push(newStreing)
     }
     const worksheet = xlsx.utils.json_to_sheet(table)
     xlsx.utils.book_append_sheet(workbook, worksheet)
@@ -244,6 +194,41 @@ app.post('/postSelectedFilters', async (req, res) => {
     catch (e) { console.log(e) }
 })
 
+app.post('/sendConsigmentBoxBerry', async (req, res) => {
+    for (let item of req.body.data) {
+        let declaredSum = 0;
+        if (item.orders.length > 1) {
+            for (let order of item.orders) {
+                declaredSum += order.declaredSum
+            }
+            if (declaredSum < 10000) declaredSum = 5
+        }
+        else declaredSum = 5
+        let options = {
+            token: boxberryToken,
+            method: "ParselCreate",
+            sdata: {
+                order_id: item.orders[0].number,
+                price: declaredSum,
+                payment_sum: item.paySum,
+                delivery_sum: item.deliverySum,
+                vid: "1",
+                shop: {
+                    name: item.codePWZ,
+                    name1: item.departurePointCode
+                },
+                customer: {
+                    fio: item.fio,
+                    phone: item.phone
+                },
+                weights: {
+                    weight: item.weightPackage
+                }
+            }
+        }
+        let res = await axios.post(`https://api.boxberry.ru/json.php?`, options)
+    }
+})
 
 async function getFilterMetadata() {
     const metadata = []
@@ -257,7 +242,6 @@ async function getFilterProject() {
     getProjects.rows.forEach(({ id, name }) => project.push({ id, name }))
     return project
 }
-
 
 
 start()
