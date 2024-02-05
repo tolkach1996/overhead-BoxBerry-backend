334acc48c4589c942d87a494171e75bfef0d82ee
Fedos16 <41968433+Fedos16@users.noreply.github.com>
2024-02-05 13:23:58 +0300

    fix calc weight

--- helpers/index.js
+++ helpers/index.js
@@ -1,29 +1,31 @@
 module.exports.calcDeclaredSum = (orders, status) => {
-    let declaredSum = orders.reduce((pre, cur) => {
-        return pre += Number(cur.declaredSum);
-    }, 0);
+	let declaredSum = orders.reduce((pre, cur) => {
+		return (pre += Number(cur.declaredSum));
+	}, 0);
 
-    if (status) return declaredSum;
+	if (status) return declaredSum;
 
-    return declaredSum < 10000 ? 5 : declaredSum;
-}
+	return declaredSum < 10000 ? 5 : declaredSum;
+};
 module.exports.calcWeight = (positions) => {
+	let summaryWeight = 0;
+	for (let position of positions) {
+		const {
+			quantity,
+			assortment: { weight },
+		} = position;
+		summaryWeight += (quantity || 0) * (weight || 0);
+	}
 
-    let summaryWeight = 0;
-    for (let position of positions) {
-        const { quantity, assortment: { weight } } = position;
-        summaryWeight += (quantity * weight);
-    }
-
-    return summaryWeight < 3000 ? 3000 : summaryWeight;
-}
+	return summaryWeight < 3000 ? 3000 : summaryWeight;
+};
 module.exports.setIssue = (openingStatus) => {
-    return openingStatus ? 1 : 0;
-}
+	return openingStatus ? 1 : 0;
+};
 module.exports.formatPhone = (tel) => {
-    const numberTel = tel.replace(/\D/g, '');
-    if (numberTel.charAt(0) != 7) {
-        return `7${numberTel.substring(1)}`;
-    }
-    return numberTel;
-}
+	const numberTel = tel.replace(/\D/g, "");
+	if (numberTel.charAt(0) != 7) {
+		return `7${numberTel.substring(1)}`;
+	}
+	return numberTel;
+};
